bd7cce79059ee166a6ac2d9819a8d92f9a02ce63
7seventh7 <m.semerukhin@gmail.com>
2022-12-20 11:26:18 +0200

    transaction added

--- MBank_v1/urls.py
+++ MBank_v1/urls.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include
-from bank.views import UserAPIView
+from django.urls import path, include, re_path
+from bank.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bank.urls')),
-    path('api/customerlist', UserAPIView.as_view())
+
+    path('api/userlist', AllUsersAPIView.as_view()),
+    path('api/userlist/<int:pk>', SingleUserAPIView.as_view()),
+
+    path('api/account/all', ListAccountAPI.as_view()),
+    path('api/acoount/user/<int:pk>', AccountAPIUser.as_view()),
+    path('api/account/admin/<int:pk>', AccauntAPIAdmin.as_view()),
+
+    path('transfer_alt/', TransactionAPI.as_view()),
+
+
+    path('api/v1/sdrf-auth/', include('rest_framework.urls')),
+    path('api/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
--- bank/serializers.py
+++ bank/serializers.py
@@ -1,8 +1,24 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from bank.models import Customer
+from bank.models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Customer
+        model = User
         fields = ('__all__')
+
+class AccountSerializer(serializers.ModelSerializer):
+
+    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    class Meta:
+        model = Account
+        fields = ('__all__')
+
+class TransferSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transfer
+        fields = ('__all__')
+
+
